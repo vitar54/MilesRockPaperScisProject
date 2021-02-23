9185240acf1b5ad7f1cecf4957efc50484c68ede
my1bitb <vrov54@gmail.com>
2021-02-23 15:53:06 -0500

    Update RockPaper.js
    made code easier to read

--- RockPaper.js
+++ RockPaper.js
@@ -5,6 +5,20 @@ const options = {
     scissors:3
 };
 
+    function GetUserText(userEnteredText) {
+        let whatUserSelected1 = 1; 
+        if (userEnteredText == "rock") {
+            whatUserSelected1 = options.rock;
+        } else if (userEnteredText == "paper") {
+            whatUserSelected1 = options.paper;
+        } else if (userEnteredText == "scissors") {
+            whatUserSelected1 = options.scissors;
+        } else {
+            whatUserSelected1 = options.scissors;
+        }
+        return whatUserSelected1;
+    }
+
         function getGameResults() {
             
             let gameNumber = 0;
@@ -28,50 +42,47 @@ const options = {
                 // ask user to choose. put results in whatUserSelected
                 let userEnteredText = "rock";
                 userEnteredText = prompt("Please select Rock, Paper or Scissors", "Rock");
-                // convert text into number
                 if ((userEnteredText != null) && (userEnteredText != "")) {
                     userEnteredText = userEnteredText.toLowerCase();
-                    if (userEnteredText == "rock") {
-                        whatUserSelected = options.rock;
-                    } else if (userEnteredText == "paper") {
-                        whatUserSelected = options.paper;
-                    } else if (userEnteredText == "scissors") {
-                        whatUserSelected = options.scissors;
-                    } else {
-                        whatUserSelected = options.scissors;
-                        userEnteredText = "scissors";
-                    }
-                } else {
+                    // convert text into number
+                    whatUserSelected = GetUserText(userEnteredText);
+                }else {
                     whatUserSelected = options.rock;
+                }
+                // define userEnteredText in case user misspelled entry
+                if (whatUserSelected == options.scissors) {
+                    userEnteredText = "scissors";
                 }
                 // generate comp choice - random number from 1 to 3
                 let compChoice = Math.floor(Math.random() * (3)) + 1 ;
                 // convert computer choice into text
-                let compText = "rock";
-                if (compChoice == options.paper) {
-                    compText = "paper";
-                } else if (compChoice == options.scissors) {
-                    compText = "scissors";
-                } else {
-                    compText = "rock";
-                }
+                let compText = getCompText(compChoice);
+                
+                // save user and computer choices
                 lineVar = "Computer: " + compText + " User: " + userEnteredText;
                 console.log(lineVar);
             
                 textResults += lineVar + "\n";
+                // compare
                 // if they are the same then Tie
                 if (compChoice == whatUserSelected) {
                     numOfDraws++;
+                // case rock paper - user wins
                 } else if (compChoice == options.rock & whatUserSelected == options.paper) {
                     numOfUserWins++;
+                // case rock scissors - user wins
                 } else if (compChoice == options.rock & whatUserSelected == options.scissors) {
                     numOfUserWins++;
+                // case paper scissors - user wins
                 } else if (compChoice == options.paper & whatUserSelected == options.scissors) {
                     numOfUserWins++;
+                // case paper rock - Computer wins
                 } else if (compChoice == options.paper & whatUserSelected == options.rock) {
                     numOfCompWins++;
+                // case scissors rock - Computer wins
                 } else if (compChoice == options.scissors & whatUserSelected == options.rock) {
                     numOfCompWins++;
+                // case scissors paper - Computer wins
                 } else if (compChoice == options.scissors & whatUserSelected == options.paper) {
                     numOfCompWins++;
                 }
@@ -84,4 +95,16 @@ const options = {
             obj.textContent = textResults + lineVar + "\n";
             
         }
+
+    function getCompText(compChoice) {
+        let compText1 = "rock";
+                if (compChoice == options.paper) {
+                    compText1 = "paper";
+                } else if (compChoice == options.scissors) {
+                    compText1 = "scissors";
+                } else {
+                    compText1 = "rock";
+                }
+            return compText1;
+    }
 
